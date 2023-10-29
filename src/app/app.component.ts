85146e2c7dbbeed3df69da17463d1bf4f991d98a
Viral-Sabhaya <113441764+Viral-Sabhaya@users.noreply.github.com>
2023-10-29 22:47:49 +0530

    add field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ang-tut';
+  name: string = '';
+  handleEvent(event: any) {
+    let a = event.target.value;
+    console.log(a);
+  }
 }
